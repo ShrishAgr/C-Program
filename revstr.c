469081db6e978a0ce8cc66012d18e553c1111142
ShrishAgr <47859800+ShrishAgr@users.noreply.github.com>
2020-03-08 15:15:39 +0530

    Add files via upload

---
+++ revstr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s,s1="",s2="";
+    int i,t;
+    printf("Enter String");
+    scanf("%c", &s);
+    t=strlen(s);
+    for(i=0;i<t;i++);
+    {
+        char x=s[i];
+        if(x==' ')
+        {
+            s1=s1+x;
+        }
+        else
+        {
+            s2=s1+s2;
+            s2=" "+s2;
+            s1="";
+        }
+    }
+    printf("Reverse of string is%c",s2);
+}
